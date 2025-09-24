e73d0647b12a00e7efc61e852a1d1f939900ccec
wahe7 <waheguru002200@gmail.com>
2025-09-25 00:55:57 +0530

    setup app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth');
+const partnerRoutes = require('./routes/partner');
+const adminRoutes = require('./routes/admin');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/partner', partnerRoutes);
+app.use('/api/admin', adminRoutes);
+
+app.get('/', (req, res) => res.send('Pixisphere Backend running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
