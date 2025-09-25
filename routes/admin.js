50bf4ec32117ed59399e2672a22f41d922d8828c
wahe7 <waheguru002200@gmail.com>
2025-09-25 14:37:56 +0530

    add routes for review , category and location

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const { getPendingVerifications, verifyPartner } = require('../controllers/adminController');
+const { getPendingVerifications, verifyPartner, getKPIs, getReviews, editReview, deleteReview,
+  createCategory, getCategories, updateCategory, deleteCategory, createLocation, getLocations, updateLocation, deleteLocation } = require('../controllers/adminController');
 const { protect } = require('../middlewares/auth');
 
 router.use(protect(['ADMIN']));
 
 router.get('/verifications', getPendingVerifications);
 router.put('/verify/:id', verifyPartner);
+
+router.get('/kpis', getKPIs);
+
+// --- Reviews ---
+router.get('/reviews', getReviews);
+router.put('/reviews/:id', editReview);
+router.delete('/reviews/:id', deleteReview);
+
+// --- Categories ---
+router.post('/categories', createCategory);
+router.get('/categories', getCategories);
+router.put('/categories/:id', updateCategory);
+router.delete('/categories/:id', deleteCategory);
+
+// --- Locations ---
+router.post('/locations', createLocation);
+router.get('/locations', getLocations);
+router.put('/locations/:id', updateLocation);
+router.delete('/locations/:id', deleteLocation);
 
 module.exports = router;
